0f60dcd44676e4a13877c9f34f5d2817febbadc1
masafumi1109 <masapon0502@gmail.com>
2023-09-28 15:29:24 +0900

    質問用に保存

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,8 +21,14 @@
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
-
+            @if(request()->is('admin*'))
+                @include('layouts.admin.navigation')
+            @elseif(request()->('owner*'))
+                @include('layouts.owner.navigation')
+            @else
+                @include('layouts.user.navigation')
+            @endif
+                
             <!-- Page Heading -->
             <header class="bg-white shadow">
                 <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
