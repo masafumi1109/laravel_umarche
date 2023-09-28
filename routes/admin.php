514a603c98d5306a1700a6c702920afd0aca90aa
masafumi1109 <masapon0502@gmail.com>
2023-09-28 17:35:33 +0900

    管理人画面エラー

--- routes/admin.php
+++ routes/admin.php
@@ -1,13 +1,13 @@
 <?php
 
-use App\Http\Controllers\User\Admin\Auth\AuthenticatedSessionController;
-use App\Http\Controllers\User\Admin\Auth\ConfirmablePasswordController;
-use App\Http\Controllers\User\Admin\Auth\EmailVerificationNotificationController;
-use App\Http\Controllers\User\Admin\Auth\EmailVerificationPromptController;
-use App\Http\Controllers\User\Admin\Auth\NewPasswordController;
-use App\Http\Controllers\User\Admin\Auth\PasswordResetLinkController;
-use App\Http\Controllers\User\Admin\Auth\RegisteredUserController;
-use App\Http\Controllers\User\Admin\Auth\VerifyEmailController;
+use App\Http\Controllers\Admin\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Admin\Auth\ConfirmablePasswordController;
+use App\Http\Controllers\Admin\Auth\EmailVerificationNotificationController;
+use App\Http\Controllers\Admin\Auth\EmailVerificationPromptController;
+use App\Http\Controllers\Admin\Auth\NewPasswordController;
+use App\Http\Controllers\Admin\Auth\PasswordResetLinkController;
+use App\Http\Controllers\Admin\Auth\RegisteredUserController;
+use App\Http\Controllers\Admin\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 /*
