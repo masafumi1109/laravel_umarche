dd8299881a7b6ca99a21d45cbd33fbe8d7c0efd1
masafumi1109 <masapon0502@gmail.com>
2023-10-24 11:19:47 +0900

    コンストラクタの修正

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -15,6 +15,18 @@ class ItemController extends Controller
     {
         $this->middleware('auth:users');
 
+        $this->middleware(function($request, $next) {
+
+            $id = $request->route()->parameter('item');
+            if(!is_null($id)) {
+                $itemId = Product::availableItems()->where('products.id', $id)->exists();
+                if (!$itemId){
+                    abort(404);
+                }
+            }
+            return $next($request);
+        });
+
     }
 
     public function index()
